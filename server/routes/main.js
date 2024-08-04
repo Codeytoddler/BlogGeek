f4be83be2f8030fc15e485c1c1c552240945f699
Saksham Rawat <saksham.r1602@gmail.com>
2024-08-04 10:54:06 +0530

    Add files via upload

--- server/routes/main.js
+++ server/routes/main.js
@@ -78,7 +78,8 @@ router.get('/post/:id', async (req, res)=>{
             title: data.title,
             description: "Simple Blog created with NodeJs, Express & MongoDb."
         }
-        res.render('post', {locals, data, currentroute: `/post/${slug}`});
+        res.render('post', {locals, data,
+            currentRoute: `/post/${slug}`});
     } catch(error){
         console.log(error);
     }
@@ -146,51 +147,51 @@ router.get('/about', (req, res)=>{
 
 
 
-function insertPostData(){
-    Post.insertMany([
-        {
-            title: "Building APIs with Node.js",
-            body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js."
-        },
-        {
-            title: "Deployment of Node.js applications",
-            body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud and containerization"
-        },
-        {
-            title: "Authentication and Authorization in Node.js",
-            body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js."
-        },
-        {
-            title: "Understand how to work with MongoDB and Mongoose",
-            body: "Understand how to work with MongoDB and Mongoose, an Object Data Modelling(ODM) library, in Node.js application."
-        },
-        {
-            title: "Build real-time, event-driven applications in Node.js",
-            body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
-        },
-        {
-            title: "Discover how to use Express.js",
-            body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
-        },
-        {
-            title: "Asynchronous Programming with Node.js",
-            body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for execution of asynchronous functions."
-        },
-        {
-            title: "Learn the basics of Node.js and its architecture",
-            body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
-        },
-        {
-            title: "NodeJs Limiting Network Traffic",
-            body: "Learn how to limit network traffic."
-        },
-        {
-            title: "Learn Morgan - HTTP Request logger for NodeJs",
-            body: "Learn Morgan"
-        }
-    ])
-}
-insertPostData();
+// function insertPostData(){
+//     Post.insertMany([
+//         {
+//             title: "Building APIs with Node.js",
+//             body: "Learn how to use Node.js to build RESTful APIs using frameworks like Express.js."
+//         },
+//         {
+//             title: "Deployment of Node.js applications",
+//             body: "Understand the different ways to deploy your Node.js applications, including on-premises, cloud and containerization"
+//         },
+//         {
+//             title: "Authentication and Authorization in Node.js",
+//             body: "Learn how to add authentication and authorization to your Node.js web applications using Passport.js."
+//         },
+//         {
+//             title: "Understand how to work with MongoDB and Mongoose",
+//             body: "Understand how to work with MongoDB and Mongoose, an Object Data Modelling(ODM) library, in Node.js application."
+//         },
+//         {
+//             title: "Build real-time, event-driven applications in Node.js",
+//             body: "Socket.io: Learn how to use Socket.io to build real-time, event-driven applications in Node.js."
+//         },
+//         {
+//             title: "Discover how to use Express.js",
+//             body: "Discover how to use Express.js, a popular Node.js web framework, to build web applications."
+//         },
+//         {
+//             title: "Asynchronous Programming with Node.js",
+//             body: "Asynchronous Programming with Node.js: Explore the asynchronous nature of Node.js and how it allows for execution of asynchronous functions."
+//         },
+//         {
+//             title: "Learn the basics of Node.js and its architecture",
+//             body: "Learn the basics of Node.js and its architecture, how it works, and why it is popular among developers."
+//         },
+//         {
+//             title: "NodeJs Limiting Network Traffic",
+//             body: "Learn how to limit network traffic."
+//         },
+//         {
+//             title: "Learn Morgan - HTTP Request logger for NodeJs",
+//             body: "Learn Morgan"
+//         }
+//     ])
+// }
+// insertPostData();
 
 
 
